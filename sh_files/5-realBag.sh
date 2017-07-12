8b54a28f2f2590a6ea65c615f5c6bcb58e187a98
harasim <jiri.harasim@gmail.com>
2017-07-12 23:20:56 +0200

    edited the launch files to reflect changes in the controller

--- sh_files/5-realBag.sh
+++ sh_files/5-realBag.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-roslaunch bebop_model realBebopBag.launch bag:=/home/harasim/catkin_ws/runtime/dso_records/SecondSet/comebackRecording04.bag
+roslaunch bebop_model realBebopBag.launch bag:=$1
+#bag:=/home/harasim/catkin_ws/runtime/obstacle_records/bigobstacle_3m02.bag
 #bag:=/home/harasim/catkin_ws/runtime/dso_records/SecondSet/comebackRecording04.bag
-
+#bag:=/home/harasim/catkin_ws/runtime/planner_records/attempt01.bag
 
 
---
+++ sh_files/7-realController.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+roslaunch action_controller actionController.launch
+
