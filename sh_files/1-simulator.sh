d6efe24c706a12e4cb84d14137e764708dae41f3
harasim <jiri.harasim@gmail.com>
2017-07-17 23:44:50 +0200

    massively improved performance, backtracker works

--- sh_files/1-simulator.sh
+++ sh_files/1-simulator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #roslaunch hector_gazebo_worlds wg_collada_realtime.launch &
-roslaunch bebop_gazebo spawnBebopWithKinect.launch 
+roslaunch bebop_gazebo spawnBebopAndWorld.launch
 
 
 
---
+++ sh_files/1-simulatorDso.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#roslaunch hector_gazebo_worlds wg_collada_realtime.launch &
+roslaunch bebop_gazebo spawnBebopAndWorld.launch spawn_with_kinect:=false use_ground_truth_for_tf:=false
+
+
+
