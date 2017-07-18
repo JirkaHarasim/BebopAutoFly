6ea6cad3999bf5f1e9cec5ca7723e465c1333858
harasim <jiri.harasim@gmail.com>
2017-07-18 02:34:12 +0200

    working boomerang

--- sh_files/3-plannerDso.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-roslaunch bebop_gazebo plan_and_execute.launch use_ground_truth_for_tf:=false &
-sleep 5
-rosrun dynamic_reconfigure dynparam set /move_group/trajectory_execution execution_duration_monitoring false
